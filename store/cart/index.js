7473fb029d9e3311ddef1b5897854dbda446154a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-01 20:00:16 +0330

    append product to cart actions and showDialog state added

--- store/cart/index.js
+++ store/cart/index.js
@@ -1,18 +1,30 @@
 const state = () => ({
   orderList: [],
-  lastOrderItem: null
+  lastOrderItem: null,
+  showAddToCart: false
 })
 
 const mutations = {
-
+  appendItemToOrderList (state, productPrice) {
+    state.lastOrderItem = productPrice
+    state.orderList.push(productPrice)
+  },
+  setShowAddToCart (state, boolValue) {
+    state.showAddToCart = boolValue
+  }
 }
 
 const actions = {
-
+  addItemToCart (vuexContext, productPrice) {
+    vuexContext.commit('appendItemToOrderList', productPrice)
+    vuexContext.commit('setShowAddToCart', true)
+  }
 }
 
 const getters = {
-
+  getShowAddToCartStatus: (state) => {
+    return state.showAddToCart
+  }
 }
 
 export default {
