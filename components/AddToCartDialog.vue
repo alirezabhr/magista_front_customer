d46bd43886ff9e7cfa587a2a80f7aa5aae9c798c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-01 20:52:45 +0330

    added to cart dialog card title changed. product price and title added

--- components/AddToCartDialog.vue
+++ components/AddToCartDialog.vue
@@ -1,32 +1,74 @@
 <template>
   <v-dialog
     v-model="show"
-    max-width="290"
+    max-width="350"
   >
     <v-card>
-      <v-card-title class="text-h5">
-        Use Google's location service?
-      </v-card-title>
+      <v-card-title class="text-body-1 green--text font-weight-bold">
+        <v-icon class="ml-1" color="green">mdi-checkbox-marked-circle-plus-outline mdi-18px</v-icon>
+        به سبد خرید اضافه شد
 
-      <v-card-text>
-        Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-      </v-card-text>
-
-      <v-card-actions>
         <v-spacer />
 
         <v-btn
-          color="green darken-1"
-          text
+          icon
+          @click.prevent="show = false"
         >
-          Disagree
+          <v-icon>mdi-close</v-icon>
         </v-btn>
+      </v-card-title>
 
-        <v-btn
-          color="green darken-1"
-          text
+      <v-card-text>
+        <v-card
+          class="pa-2"
+          outlined
         >
-          Agree
+          <v-row>
+            <v-col cols="3">
+              <v-card
+                outlined
+              >
+                <v-img
+                  :aspect-ratio="1"
+                  :src="getImageUrl"
+                >
+                  <template #placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center"
+                    >
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-2"
+                      />
+                    </v-row>
+                  </template>
+                </v-img>
+              </v-card>
+            </v-col>
+            <v-col cols="7">
+              <v-row no-gutters>
+                <div class="text-truncate">
+                  {{ getLastAppendedProduct.product.title }}
+                </div>
+              </v-row>
+              <v-row class="font-weight-bold" no-gutters>
+                {{ getLastAppendedProduct.price }} تومان
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn
+          class="darken-1 white--text px-4 mx-auto"
+          color="green"
+          rounded
+        >
+          <v-icon>mdi-cart mdi-flip-h</v-icon>
+          رفتن به سبد
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -34,13 +76,20 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
+import { mapMutations, mapGetters } from 'vuex'
 
 export default {
   name: 'AddToCartDialog',
   data () {
     return {
       show: true
+    }
+  },
+  computed: {
+    ...mapGetters('cart', ['getLastAppendedProduct']),
+
+    getImageUrl () {
+      return process.env.baseURL + this.getLastAppendedProduct.product.display_image
     }
   },
   watch: {
