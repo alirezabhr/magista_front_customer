b63d8ef4c5cd4f6a08c619dbe192dcbe4ce15b50
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-01 18:06:57 +0330

    cart store created

---
+++ store/cart/index.js
@@ -0,0 +1,24 @@
+const state = () => ({
+  orderList: [],
+  lastOrderItem: null
+})
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  getters,
+  actions
+}
