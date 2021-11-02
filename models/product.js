4645e0eda61108e00937c392821cd2cdcc004821
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-02 10:59:36 +0330

    product model created

---
+++ models/product.js
@@ -0,0 +1,12 @@
+class Product {
+  constructor (price, shortcode, title, description, displayImage, rate) {
+    this.price = price
+    this.shortcode = shortcode
+    this.title = title
+    this.description = description
+    this.displayImage = displayImage
+    this.rate = rate
+  }
+}
+
+export default Product
