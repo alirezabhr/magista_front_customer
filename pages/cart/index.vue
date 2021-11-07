4977aed42f14bcb47a9f39ddf24950262c70f0c8
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-07 17:10:26 +0330

    cart page cards added

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -1,14 +1,13 @@
 <template>
   <v-row justify="center">
     <v-col cols="12" sm="9" md="8" lg="6" class="pa-0">
-      <v-card min-height="620">
-          sadfsd
-        <!-- <v-card v-for="seller in getSellersList" :key="seller.instagram_username">
+      <v-card min-height="620" class="pa-2">
+        <v-card v-for="shopOrder in getOrderList" :key="shopOrder.shop" class="pa-2">
           <v-card-title>
-            خرید از {{ seller.instagram_username }}
+            خرید از {{ shopOrder.shop }}
           </v-card-title>
 
-          <v-card v-for="product in seller.products" :key="product.id" outlined>
+          <v-card v-for="order in shopOrder.ordersList" :key="order.product.shortcode" outlined class="mb-2">
             <v-row class="pa-2">
               <v-col cols="3">
                 <v-card
@@ -16,7 +15,7 @@
                 >
                   <v-img
                     :aspect-ratio="1"
-                    :src="getImageUrl"
+                    :src="getFullImageUrl(order.product.displayImageUrl)"
                   >
                     <template #placeholder>
                       <v-row
@@ -33,26 +32,76 @@
                   </v-img>
                 </v-card>
               </v-col>
-              <v-col cols="9">
+              <v-col cols="1">
+                <v-row class="font-weight-bold" no-gutters>
+                  {{ order.count }}X
+                </v-row>
+              </v-col>
+              <v-col cols="7">
                 <v-row no-gutters>
                   <div class="text-truncate">
-                    {{ getLastAppendedProduct.product.title }}
+                    {{ order.product.title }}
                   </div>
                 </v-row>
                 <v-row class="font-weight-bold" no-gutters>
-                  {{ getLastAppendedProduct.price }} تومان
+                  {{ order.product.price }} تومان
                 </v-row>
               </v-col>
             </v-row>
           </v-card>
-        </v-card> -->
+          <v-card-actions>
+            <v-col>
+              <div>
+                 تعداد کل: {{ totalCount(shopOrder.ordersList) }}
+              </div>
+              <div>
+                قیمت کل: {{ totalPrice(shopOrder.ordersList) }} تومان
+              </div>
+            </v-col>
+          </v-card-actions>
+        </v-card>
+        <v-card-actions>
+          <v-btn
+            class="darken-1 white--text px-4 mx-auto"
+            color="green"
+            rounded
+            @click.prevent=""
+          >
+            تکمیل خرید
+          </v-btn>
+        </v-card-actions>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'CartPage'
+  name: 'CartPage',
+  methods: {
+    getFullImageUrl (src) {
+      return process.env.baseURL + src
+    },
+    totalPrice (ordersList) {
+      let total = 0
+      ordersList.forEach((order) => {
+        const orderPrice = order.count * order.product.price
+        total += orderPrice
+      });
+      return total
+    },
+    totalCount (orderList) {
+      let total = 0
+      orderList.forEach((order) => {
+        total += order.count
+      })
+      return total
+    }
+  },
+  computed: {
+    ...mapGetters('cart', ['getOrderList'])
+  }
 }
 </script>
