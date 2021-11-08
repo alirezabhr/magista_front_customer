f9251772a1ea72bcc31d69854f80a5c738fa8d09
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-08 19:46:37 +0330

    Customer Form completed

---
+++ components/CustomerForm.vue
@@ -0,0 +1,158 @@
+<template>
+    <v-card>
+        <v-row class="pa-5 ma-2 red--text text--darken-3">
+            <v-icon color="red" class="px-3">mdi-alert-decagram mdi-22px</v-icon>
+            <span>ابتدا باید حساب کاربری خود را تکمیل کنید</span>
+        </v-row>
+        <v-form
+            ref="form"
+            lazy-validation
+            @submit.prevent="validateAndSubmitForm"
+        >
+        <v-card-title>
+            <v-col>
+            <div class="text-h6 font-weight-bold">ثبت اطلاعات</div>
+            </v-col>
+        </v-card-title>
+
+        <v-col class="py-0 px-8">
+            <v-text-field
+                v-model="customerData.name"
+                label="نام و نام خانوادگی"
+                :rules="nameRules"
+                outlined
+                dense
+            />
+            <v-row>
+            <v-col cols="12" md="6" class="my-0 py-0">
+                <v-autocomplete
+                    v-model="customerData.province"
+                    label="استان"
+                    :items="getAllProvinces"
+                    :rules="provinceRules"
+                    outlined
+                    dense
+                >
+                <template #no-data>
+                    <v-list-item>
+                    <v-list-item-title>
+                        هیچ استانی با این مشخصات پیدا نشد.
+                    </v-list-item-title>
+                    </v-list-item>
+                </template>
+                </v-autocomplete>
+            </v-col>
+
+            <v-col cols="12" md="6" class="my-0 py-0">
+                <v-autocomplete
+                    v-model="customerData.city"
+                    label="شهر"
+                    :items="getProvinceCities"
+                    :rules="cityRules"
+                    outlined
+                    dense
+                >
+                <template #no-data>
+                    <v-list-item>
+                    <v-list-item-title>
+                        هیچ شهری با این مشخصات پیدا نشد.
+                    </v-list-item-title>
+                    </v-list-item>
+                </template>
+                </v-autocomplete>
+            </v-col>
+            </v-row>
+            <v-textarea
+                v-model="customerData.address"
+                label="آدرس"
+                :rules="addressRules"
+                outlined
+                dense
+            />
+        </v-col>
+
+        <v-card-actions class="px-8">
+            <v-col cols="12">
+                <v-row justify="center">
+                    <v-btn
+                        block
+                        class="primary font-weight-bold py-5"
+                        :loading="isSubmittingForm"
+                        type="submit"
+                    >
+                        ثبت و ادامه
+                    </v-btn>
+                </v-row>
+                <v-row justify="center">
+                    <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="$emit('close')"
+                    >
+                        بستن
+                    </v-btn>
+                </v-row>
+            </v-col>
+        </v-card-actions>
+        
+        </v-form>
+    </v-card>
+</template>
+
+<script>
+import provincesAndCities from '@/assets/json/iran_cities.json'
+
+export default {
+  name: 'CustomerForm',
+  props: {
+    isSubmittingForm: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+      customerData: {
+        name: '',
+        province: '',
+        city: '',
+        address: ''
+      },
+      nameRules: [
+        value => !!value || '.نام و نام‌خانوادگی الزامی می‌باشد',
+      ],
+      provinceRules: [
+        value => !!value || 'استان الزامی است.',
+        value => this.getAllProvinces.includes(value) || 'استان نامعتبر است.'
+      ],
+      cityRules: [
+        value => !!value || 'شهر الزامی است.',
+        value => this.getProvinceCities.includes(value) || 'شهر نامعتبر است.'
+      ],
+      addressRules: [
+        value => !!value || 'آدرس الزامی می‌باشد.',
+        value => !/^\s+$/.test(value) || 'آدرس نامعتبر است.'
+      ]
+    }
+  },
+  computed: {
+    getAllProvinces () {
+      return Array.from(provincesAndCities, item => item.name)
+    },
+    getProvinceCities () {
+      if (this.getAllProvinces.includes(this.customerData.province)) {
+        const province = provincesAndCities.find(province => province.name === this.customerData.province)
+        return Array.from(province.cities, item => item.name)
+      }
+      return []
+    }
+  },
+  methods: {
+    validateAndSubmitForm () {
+      if (this.$refs.form.validate()) {
+        this.$emit('submit', this.customerData)
+      }
+    }
+  }
+}
+</script>
