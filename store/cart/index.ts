a1aa70319d2d94d140849471f7b6a93c17c4a959
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-10 12:30:31 +0330

    ordersList with type OrderItem[] changed to cart with ShopOrder[]

--- store/cart/index.ts
+++ store/cart/index.ts
@@ -2,18 +2,17 @@ import { GetterTree, MutationTree, ActionTree } from "vuex"
 import { RootState } from '../index'
 import Product from "~/models/product"
 import OrderItem from "~/models/order_item"
-import Shop from "~/models/shop"
-import shop from "../shop"
+import ShopOrder from "~/models/shop_order"
 
 const namespace = 'cart'
 
 interface CartState {
-  ordersList: OrderItem[]
+  cart: ShopOrder[]
   lastAppendedProduct: Product | null
 }
 
 const state = () : CartState => ({
-  ordersList: [],
+  cart: [],
   lastAppendedProduct: null
 })
 
@@ -25,31 +24,48 @@ const mutations = <MutationTree<CartState>>{
     // initial cart
     const strCart = localStorage.getItem('cart')
     if (strCart) {
-      state.ordersList = JSON.parse(strCart)
+      state.cart = JSON.parse(strCart)
     }
   },
   setLocalStorageOrderList (state) {
-    localStorage.setItem('cart', JSON.stringify(state.ordersList))
+    localStorage.setItem('cart', JSON.stringify(state.cart))
   },
   appendItemToOrderList (state, product: Product) {
     state.lastAppendedProduct = product
-    const orderIndex = state.ordersList.findIndex((el: OrderItem) => el.product === product)
-    if (orderIndex === -1) {  // this product doesn't exist in ordersList
-      state.ordersList.push(new OrderItem(product))
-    } else {
-      state.ordersList[orderIndex].count += 1
+
+    const shopOrderIndex = state.cart.findIndex((el: ShopOrder) => el.shop_id === product.shop.id)
+    if (shopOrderIndex === -1) {  // don't have any products from this shop
+      const shopOrder = new ShopOrder(product.shop.id, product.shop.instagramUsername)
+      shopOrder.addItem(new OrderItem(product))
+      state.cart.push(shopOrder)
+    } else {  // have some products from this shop
+      const orderItemIndex = state.cart[shopOrderIndex].orders.findIndex(
+        (order: OrderItem) => order.product === product)
+
+      if (orderItemIndex === -1) {  // this product does not exist in the cart
+        const orderItem = new OrderItem(product)
+        state.cart[shopOrderIndex].orders.push(orderItem)
+      } else {  // you had ordered this product before
+        state.cart[shopOrderIndex].orders[orderItemIndex].count += 1
+      }
     }
   },
   removeItemFromOrderList (state, product: Product) {
-    const orderIndex = state.ordersList.findIndex((el: OrderItem) => el.product === product)
-    if (orderIndex === -1) {  // this product doesn't exist in ordersList
-      // should throw error
-      console.log('ERROR in removing item from orders list')
-    } else {
-      if (state.ordersList[orderIndex].count > 1) {
-        state.ordersList[orderIndex].count -= 1
-      } else {
-        state.ordersList.splice(orderIndex, 1)
+    const shopOrderIndex = state.cart.findIndex((el: ShopOrder) => el.shop_id === product.shop.id)
+    if (shopOrderIndex === -1) {  // don't have any products from this shop
+      console.log('ERROR Occured in removing item from cart') // will throw an erro
+    } else {  // have some products from this shop
+      const orderItemIndex = state.cart[shopOrderIndex].orders.findIndex(
+        (order: OrderItem) => order.product === product)
+
+      if (orderItemIndex === -1) {  // this product does not exist in the cart
+        console.log('ERROR Occured in removing item from cart') // will throw an erro
+      } else {  // you had ordered this product before
+        if (state.cart[shopOrderIndex].orders[orderItemIndex].count > 1) {
+          state.cart[shopOrderIndex].orders[orderItemIndex].count -= 1
+        } else {
+          state.cart[shopOrderIndex].orders.splice(orderItemIndex, 1)
+        }
       }
     }
   },
@@ -72,29 +88,24 @@ const getters = <GetterTree<CartState, RootState>>{
   },
   getCartItemCounts: (state) => {
     let count = 0
-    state.ordersList.forEach((order: OrderItem) => {
-      count += order.count
+    state.cart.forEach((shopOrder: ShopOrder) => {
+      shopOrder.orders.forEach((orderItem: OrderItem) => {
+        count += orderItem.count
+      })
     })
     return count
   },
-  getOrdersShopList: (state) : Shop[] => {
-    let shopsList: Shop[] = []
-    state.ordersList.forEach((order: OrderItem) => {
-      if (shopsList.findIndex(shop => shop.id === order.product.shop.id) === -1) {
-        shopsList.push(order.product.shop)
-      }
-    })
-    return shopsList
-  },
-  getOrdersList: (state) : OrderItem[] => {
-    return state.ordersList
-  },
-  getCartTotalPrice: (state) : number => {
+  getCartTotalPrice: (state) => {
     let total = 0
-    state.ordersList.forEach((order: OrderItem) => {
-      total = total + order.count * order.product.price
+    state.cart.forEach((shopOrder: ShopOrder) => {
+      shopOrder.orders.forEach((orderItem: OrderItem) => {
+        total = total + orderItem.count * orderItem.product.price
+      })
     })
     return total
+  },
+  getCart: (state) : ShopOrder[] => {
+    return state.cart
   }
 }
 
