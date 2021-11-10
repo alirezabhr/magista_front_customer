26adaf89aad9da263b1a0fec87bc871f16b2e72a
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-10 13:45:54 +0330

    payment process actions added

--- store/cart/index.ts
+++ store/cart/index.ts
@@ -3,17 +3,20 @@ import { RootState } from '../index'
 import Product from "~/models/product"
 import OrderItem from "~/models/order_item"
 import ShopOrder from "~/models/shop_order"
+import Invoice from "~/models/invoice"
 
 const namespace = 'cart'
 
 interface CartState {
   cart: ShopOrder[]
   lastAppendedProduct: Product | null
+  paymentPendingInvoices: Invoice[]
 }
 
 const state = () : CartState => ({
   cart: [],
-  lastAppendedProduct: null
+  lastAppendedProduct: null,
+  paymentPendingInvoices: []
 })
 
 const mutations = <MutationTree<CartState>>{
@@ -53,13 +56,13 @@ const mutations = <MutationTree<CartState>>{
   removeItemFromOrderList (state, product: Product) {
     const shopOrderIndex = state.cart.findIndex((el: ShopOrder) => el.shop_id === product.shop.id)
     if (shopOrderIndex === -1) {  // don't have any products from this shop
-      console.log('ERROR Occured in removing item from cart') // will throw an erro
+      console.log('ERROR Occured in removing item from cart') // TODO throw an erro
     } else {  // have some products from this shop
       const orderItemIndex = state.cart[shopOrderIndex].orders.findIndex(
         (order: OrderItem) => order.product === product)
 
       if (orderItemIndex === -1) {  // this product does not exist in the cart
-        console.log('ERROR Occured in removing item from cart') // will throw an erro
+        console.log('ERROR Occured in removing item from cart') // TODO throw an erro
       } else {  // you had ordered this product before
         if (state.cart[shopOrderIndex].orders[orderItemIndex].count > 1) {
           state.cart[shopOrderIndex].orders[orderItemIndex].count -= 1
@@ -69,6 +72,13 @@ const mutations = <MutationTree<CartState>>{
       }
     }
   },
+  clearCart (state) {
+    state.cart = []
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  },
+  setPaymentPendingInvoices (state, invoices: Invoice[]) {
+    state.paymentPendingInvoices = invoices
+  }
 }
 
 const actions = <ActionTree<CartState, RootState>>{
@@ -80,6 +90,37 @@ const actions = <ActionTree<CartState, RootState>>{
     vuexContext.commit('removeItemFromOrderList', product)
     vuexContext.commit('setLocalStorageOrderList')
   },
+  createCartInvoices (vuexContext) {
+    const url = process.env.baseURL + 'shop/cart/'
+
+    let payload: {
+      customer_id: number
+      cart: ShopOrder[]
+    }
+    payload = {
+      customer_id: vuexContext.rootGetters['auth/getCustomerId'],
+      cart: vuexContext.getters.getCart
+    }
+
+    return this.$client.post(url, payload).then((response) => {
+      vuexContext.commit('setPaymentPendingInvoices', response.data)
+      vuexContext.commit('clearCart')
+    }).catch((e) => {
+      throw e.response
+    })
+  },
+  payPendingInvoices (vuexContext) {
+    const url = process.env.baseURL + 'shop/cart/'
+    const payload = vuexContext.getters.getPaymentPendingInvoices
+    console.log('in payPendingInvoices')
+    console.log(payload)
+
+    return this.$client.put(url, payload).then((response) => {
+      console.log('paid succussfully')
+    }).catch((e) => {
+      throw e.response
+    })
+  }
 }
 
 const getters = <GetterTree<CartState, RootState>>{
@@ -106,6 +147,9 @@ const getters = <GetterTree<CartState, RootState>>{
   },
   getCart: (state) : ShopOrder[] => {
     return state.cart
+  },
+  getPaymentPendingInvoices: (state) : Invoice[] => {
+    return state.paymentPendingInvoices
   }
 }
 
