58a8c745a5f1e80bace5bb12587ecdb97719e94b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-16 15:51:40 +0330

    nuxt-ts removed

--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,53 @@
   <v-row justify="center" align="center">
     <v-col cols="12">
       <AddToCartDialog v-if="showDialog" @closeDialog="showDialog=false" />
-      <v-row class="pa-2">
-        <v-layout style="overflow-x: scroll;" class="pa-4">
-          <ProductPreviewCard
-            v-for="product in bestSellerProducts"
-            :key="product.shortcode"
-            shop-name="test online shop"
-            :image-url="product.displayImageUrl"
-            :title="product.title"
-            :rate="product.rate"
-            :price="product.price"
-            @addToCart="addToCart(product)"
-          />
-        </v-layout>
+
+      <v-row>
+        <v-col cols="12" md="8">
+          <v-card outlined rounded="xl">
+            <v-img 
+              :src="saleImageUrls[0]"
+            />
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-row no-gutters>
+            <v-col cols="6" md="12" class="pa-2">
+              <v-card outlined rounded="xl">
+                <v-img 
+                  :src="saleImageUrls[1]"
+                />
+              </v-card>
+            </v-col>
+            <v-col cols="6" md="12" class="pa-2">
+              <v-card outlined rounded="xl">
+                <v-img 
+                  :src="saleImageUrls[1]"
+                />
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
       </v-row>
+      <v-card outlined class="pa-5 my-2" rounded="lg">
+        <v-card-title class="pt-0 mt-0 red--text">
+          فروش ویژه
+        </v-card-title>
+        <v-row>
+          <v-layout style="overflow-x: scroll;" class="pa-4">
+            <ProductPreviewCard
+              v-for="product in bestSellerProducts"
+              :key="product.shortcode"
+              shop-name="test online shop"
+              :image-url="product.displayImageUrl"
+              :title="product.title"
+              :rate="product.rate"
+              :price="product.price"
+              @addToCart="addToCart(product)"
+            />
+          </v-layout>
+        </v-row>
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -37,6 +70,12 @@ export default {
     return {
       bestSellerProducts: bestSellerMock,
       showDialog: false
+    }
+  },
+  computed: {
+    saleImageUrls () {
+      const base = process.env.baseURL + 'media/source/'
+      return [`${base}autumn_sale.png`, `${base}sale1.png`]
     }
   },
   methods: {
