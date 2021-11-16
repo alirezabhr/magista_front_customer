f304ca5932de62e05b2e2d560593546e46817410
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-16 15:52:22 +0330

    nuxt link added for magista title

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,14 +7,21 @@
       elevation="3"
     >
       <v-toolbar-title class="primary--text font-weight-bold mx-3">
-        مگیستا
+        <NuxtLink to="/" active-class="text-decoration-none">
+          مگیستا
+        </NuxtLink>
       </v-toolbar-title>
 
       <v-spacer />
 
-      <v-btn text>
+      <v-btn text to="/auth" v-if="!isAuthenticated">
         ورود / ثبت‌نام
       </v-btn>
+
+      <v-btn text v-else>
+        پروفایل
+      </v-btn>
+
       <v-divider vertical class="mx-3" />
       <v-btn text to="/cart">
         <v-badge
@@ -22,12 +29,13 @@
           :value="getCartItemCounts"
           color="green"
           overlap
+          left
         >
-          سبد
-          <v-icon>
+          <v-icon class="pr-1">
             mdi-cart-variant mdi-flip-h
           </v-icon>
         </v-badge>
+          سبد
       </v-btn>
       <v-divider vertical class="mx-3" />
       <v-btn rounded outlined class="primary--text" href="https://vendor.magista.ir" target="_blank">
@@ -49,6 +57,7 @@ import { mapMutations, mapGetters } from 'vuex'
 export default {
   name: 'DefaultLayout',
   computed: {
+    ...mapGetters('auth', ['isAuthenticated']),
     ...mapGetters('cart', ['getCartItemCounts'])
   },
   mounted () {
