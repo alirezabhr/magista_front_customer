ae8c9a7ce1d203ff322c0bf79319aa539e870fe7
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-18 18:34:08 +0330

    spaces format changed

--- components/CartOrderItem.vue
+++ components/CartOrderItem.vue
@@ -1,74 +1,69 @@
 <template>
   <v-card outlined class="mb-2">
     <v-row class="pa-2">
-        <v-col cols="3">
-            <v-card
-            outlined
-            >
-            <v-img
-                :aspect-ratio="1"
-                :src="getFullImageUrl(orderItem.product.displayImageUrl)"
-            >
-                <template #placeholder>
-                <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                >
-                    <v-progress-circular
-                    indeterminate
-                    color="grey lighten-2"
-                    />
-                </v-row>
-                </template>
-            </v-img>
-            </v-card>
-        </v-col>
-        <v-col cols="1" class="" align-self="center">
-            <v-row justify="center">
-            <v-btn icon @click.prevent="addItemToCart(orderItem.product)"><v-icon>mdi-plus</v-icon></v-btn>
-            </v-row>
-            <v-row class="font-weight-bold" justify="center">
-            {{ orderItem.count }}
-            </v-row>
-            <v-row justify="center">
-            <v-btn icon @click.prevent="removeItemFromCart(orderItem.product)"><v-icon>mdi-minus</v-icon></v-btn>
-            </v-row>
-        </v-col>
-        <v-col cols="7">
-            <v-row no-gutters>
-            <div class="text-truncate">
-                {{ orderItem.product.title }}
-            </div>
-            </v-row>
-            <v-row class="font-weight-bold" no-gutters>
-            {{ orderItem.product.price }} تومان
-            </v-row>
-        </v-col>
+      <v-col cols="3">
+        <v-card outlined>
+          <v-img
+            :aspect-ratio="1"
+            :src="getFullImageUrl(orderItem.product.displayImageUrl)"
+          >
+            <template #placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular indeterminate color="grey lighten-2" />
+              </v-row>
+            </template>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="1" class="" align-self="center">
+        <v-row justify="center">
+          <v-btn icon @click.prevent="addItemToCart(orderItem.product)"
+            ><v-icon>mdi-plus</v-icon></v-btn
+          >
+        </v-row>
+        <v-row class="font-weight-bold" justify="center">
+          {{ orderItem.count }}
+        </v-row>
+        <v-row justify="center">
+          <v-btn icon @click.prevent="removeItemFromCart(orderItem.product)"
+            ><v-icon>mdi-minus</v-icon></v-btn
+          >
+        </v-row>
+      </v-col>
+      <v-col cols="7">
+        <v-row no-gutters>
+          <div class="text-truncate">
+            {{ orderItem.product.title }}
+          </div>
+        </v-row>
+        <v-row class="font-weight-bold" no-gutters>
+          {{ orderItem.product.price }} تومان
+        </v-row>
+      </v-col>
     </v-row>
   </v-card>
 </template>
 
 <script lang="ts">
-import { PropType } from 'vue'
-import { mapActions } from 'vuex'
+import { PropType } from "vue";
+import { mapActions } from "vuex";
 
-import OrderItem from '~/models/order_item'
+import OrderItem from "~/models/order_item";
 
 export default {
-    name: 'CartOrderItem',
-    props: {
-        orderItem: {
-            type: Object as PropType<OrderItem>,
-            required: true
-        }
+  name: "CartOrderItem",
+  props: {
+    orderItem: {
+      type: Object as PropType<OrderItem>,
+      required: true,
     },
-    methods: {
-        ...mapActions('cart', ['addItemToCart', 'removeItemFromCart']),
+  },
+  methods: {
+    ...mapActions("cart", ["addItemToCart", "removeItemFromCart"]),
 
-        getFullImageUrl (src:string) {
-            return process.env.baseURL + src
-        },
-    }
-}
+    getFullImageUrl(src: string) {
+      return process.env.baseURL + src;
+    },
+  },
+};
 </script>
--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,30 @@
 <template>
-  <v-footer
-    padless
-  >
-    <v-card
-      flat
-      tile
-      class="indigo lighten-1 white--text text-center"
-    >
-
+  <v-footer padless>
+    <v-card flat tile class="indigo lighten-1 white--text text-center">
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
         <v-row>
-            <v-col cols="6">
-                <v-row>
-                    <v-col cols="3" md="2">
-                        <a referrerpolicy="origin" target="_blank" href="https://trustseal.enamad.ir/?id=237568&amp;Code=eFDsUbAr6MywnIzE6rMI">
-                            <img referrerpolicy="origin" src="https://Trustseal.eNamad.ir/logo.aspx?id=237568&amp;Code=eFDsUbAr6MywnIzE6rMI" alt="" style="cursor:pointer" id="eFDsUbAr6MywnIzE6rMI" />
-                        </a>
-                    </v-col>
-                </v-row>    
-            </v-col>
-        </v-row>    
+          <v-col cols="6">
+            <v-row>
+              <v-col cols="3" md="2">
+                <a
+                  referrerpolicy="origin"
+                  target="_blank"
+                  href="https://trustseal.enamad.ir/?id=237568&amp;Code=eFDsUbAr6MywnIzE6rMI"
+                >
+                  <img
+                    referrerpolicy="origin"
+                    src="https://Trustseal.eNamad.ir/logo.aspx?id=237568&amp;Code=eFDsUbAr6MywnIzE6rMI"
+                    alt=""
+                    style="cursor: pointer"
+                    id="eFDsUbAr6MywnIzE6rMI"
+                  />
+                </a>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -29,6 +32,6 @@
 
 <script>
 export default {
-    name: 'Footer'
-}
+  name: "Footer",
+};
 </script>
