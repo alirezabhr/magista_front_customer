db2befae24fecd14aa70b029a3e02dd2c5813d68
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 11:30:45 +0330

    discount percent, description and original price added in model attributes

--- models/product.ts
+++ models/product.ts
@@ -4,23 +4,30 @@ class Product {
   id: number
   shop: Shop
   finalPrice: number
+  originalPrice: number
   shortcode: string
   title: string
   description: string
   displayImageUrl: string
   rate: number | null
+  discountPercent: number | null
+  discountDescription: string
 
-  constructor(id: number, shop: Shop, finalPrice: number, shortcode: string,
-    title: string, description: string, displayImage: string, rate: number | null) {
+  constructor(id: number, shop: Shop, finalPrice: number, originalPrice: number, shortcode: string,
+    title: string, description: string, displayImage: string, rate: number | null, discountPercent: number | null,
+    discountDescription: string) {
 
     this.id = id
     this.shop = shop
     this.finalPrice = finalPrice
+    this.originalPrice = originalPrice
     this.shortcode = shortcode
     this.title = title
     this.description = description
     this.displayImageUrl = displayImage
     this.rate = rate
+    this.discountPercent = discountPercent
+    this.discountDescription = discountDescription
   }
 
   toJSON() {
