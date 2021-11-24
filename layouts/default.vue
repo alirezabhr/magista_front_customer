3d2711159c125cb8e39f1b1bc691a9f171ce4c3e
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 11:31:21 +0330

    header added as a component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,66 +1,26 @@
 <template>
   <v-app>
-    <v-app-bar
-      fixed
-      app
-      class="white"
-      elevation="3"
-    >
-      <v-toolbar-title class="primary--text font-weight-bold mx-3">
-        <NuxtLink to="/" active-class="text-decoration-none">
-          مگیستا
-        </NuxtLink>
-      </v-toolbar-title>
-
-      <v-spacer />
-
-      <v-btn text to="/auth" v-if="!isAuthenticated">
-        ورود / ثبت‌نام
-      </v-btn>
-
-      <v-btn text v-else>
-        پروفایل
-      </v-btn>
-
-      <v-divider vertical class="mx-3" />
-      <v-btn text to="/cart">
-        <v-badge
-          :content="getCartItemCounts"
-          :value="getCartItemCounts"
-          color="green"
-          overlap
-          left
-        >
-          <v-icon class="pr-1">
-            mdi-cart-variant mdi-flip-h
-          </v-icon>
-        </v-badge>
-          سبد
-      </v-btn>
-      <v-divider vertical class="mx-3" />
-      <v-btn rounded outlined class="primary--text" href="https://vendor.magista.ir" target="_blank">
-        پنل فروشنده
-      </v-btn>
-    </v-app-bar>
-
+    <Header />
     <v-main class="background">
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
-
     <Footer />
   </v-app>
 </template>
 
 <script>
-import { mapMutations, mapGetters } from 'vuex'
+import { mapMutations } from 'vuex'
+
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 
 export default {
   name: 'DefaultLayout',
-  computed: {
-    ...mapGetters('auth', ['isAuthenticated']),
-    ...mapGetters('cart', ['getCartItemCounts'])
+  components: {
+    Header,
+    Footer
   },
   mounted () {
     this.initialCartStore()
