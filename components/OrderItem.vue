ee0972690c31cdee7c0ce60be354c1ae4136f8db
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 20:24:09 +0330

    OrderItem component created

---
+++ components/OrderItem.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-col cols="11" class="pa-0">
+    <v-card outlined class="mb-2">
+      <v-row class="pa-2">
+        <v-col cols="3">
+          <v-card outlined>
+            <v-img
+              :aspect-ratio="1"
+              :src="getFullImageUrl(order.product.displayImage)"
+            >
+              <template #placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular indeterminate color="grey lighten-2" />
+                </v-row>
+              </template>
+            </v-img>
+          </v-card>
+        </v-col>
+        <v-col cols="1" class="" align-self="center">
+          <v-row class="font-weight-bold" justify="center">
+            ×{{ order.count }}
+          </v-row>
+        </v-col>
+        <v-col cols="7">
+          <v-row no-gutters>
+            <div class="text-truncate">
+              {{ order.product.title }}
+            </div>
+          </v-row>
+          <v-row class="font-weight-bold" no-gutters>
+            {{ order.price }} تومان
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-col>
+</template>
+
+<script lang="ts">
+import { PropType } from 'vue'
+import OrderItem from '~/models/order_item'
+
+export default {
+  name: 'OrderItem',
+  props: {
+    order: {
+      type: Object as PropType<OrderItem>,
+      required: true
+    }
+  },
+  methods: {
+    getFullImageUrl (src: string) {
+      return process.env.baseURL + src
+    }
+  }
+}
+</script>
