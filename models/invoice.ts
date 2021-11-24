91d12c9817ffd0827690801921253e124dec4417
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-24 20:23:55 +0330

    invoice status and status text added

--- models/invoice.ts
+++ models/invoice.ts
@@ -1,18 +1,28 @@
+import OrderItem from './order_item'
+import Shop from './shop'
+import Customer from './customer'
+
+const InvoiceStatus = ['در انتظار پرداخت', 'پرداخت شده', 'ارسال شده', 'دریافت شده', 'لغو شده']
+
 class Invoice {
     id: number
     status: number
-    shop: number
-    customer: number
+    statusText: string
+    shop: Shop
+    customer: Customer
+    orders: OrderItem[]
     updatedAt: Date
     createdAt: Date
 
-    constructor (id: number, status: number, shop: number, customer: number, updatedAt: Date, createdAt: Date) {
-        this.id = id
-        this.status = status
-        this.shop = shop
-        this.customer = customer
-        this.updatedAt = updatedAt
-        this.createdAt = createdAt
+    constructor (id: number, status: number, shop: Shop, customer: Customer, orders: OrderItem[], updatedAt: Date, createdAt: Date) {
+      this.id = id
+      this.status = status
+      this.statusText = InvoiceStatus[status - 1]
+      this.shop = shop
+      this.customer = customer
+      this.orders = orders
+      this.updatedAt = updatedAt
+      this.createdAt = createdAt
     }
 }
 
