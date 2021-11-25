f6208114d3b97553fc9dceb782168dbd0caf2c4b
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-25 15:11:07 +0330

    orders changed to orderItems

--- models/invoice.ts
+++ models/invoice.ts
@@ -10,17 +10,17 @@ class Invoice {
     statusText: string
     shop: Shop
     customer: Customer
-    orders: OrderItem[]
+    orderItems: OrderItem[]
     updatedAt: Date
     createdAt: Date
 
-    constructor (id: number, status: number, shop: Shop, customer: Customer, orders: OrderItem[], updatedAt: Date, createdAt: Date) {
+    constructor (id: number, status: number, shop: Shop, customer: Customer, orderItems: OrderItem[], updatedAt: Date, createdAt: Date) {
       this.id = id
       this.status = status
       this.statusText = InvoiceStatus[status - 1]
       this.shop = shop
       this.customer = customer
-      this.orders = orders
+      this.orderItems = orderItems
       this.updatedAt = updatedAt
       this.createdAt = createdAt
     }
