5ed9e0fb935a77e3dd57e58f004ccee9530d95f2
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-11-25 12:34:32 +0330

    customer id added in constructor

--- models/customer.ts
+++ models/customer.ts
@@ -1,15 +1,17 @@
 class Customer {
-    user: number
+    id: number
     name: string
     province: string
     city: string
     address: string
 
-    constructor(userId: number, name: string, province: string, city: string, address: string) {
-        this.user = userId
-        this.name = name
-        this.province = province
-        this.city = city
-        this.address = address
+    constructor (id: number, name: string, province: string, city: string, address: string) {
+      this.id = id
+      this.name = name
+      this.province = province
+      this.city = city
+      this.address = address
     }
 }
+
+export default Customer
