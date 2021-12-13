a3e3b850e3e5498ca20944febd2820b62bb02155
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-13 12:49:01 +0330

    product attribute model created

--- models/product.ts
+++ models/product.ts
@@ -1,4 +1,5 @@
-import Shop from "./shop"
+import ProductAttribute from './product_attribute'
+import Shop from './shop'
 
 class Product {
   id: number
@@ -13,10 +14,11 @@ class Product {
   isExisting: boolean
   discountPercent: number | null
   discountDescription: string
+  attributes: ProductAttribute[]
 
   constructor(id: number, shop: Shop, finalPrice: number, originalPrice: number, shortcode: string,
     title: string, description: string, displayImage: string, rate: number | null, isExisting: boolean,
-    discountPercent: number | null, discountDescription: string) {
+    discountPercent: number | null, discountDescription: string, attributes: ProductAttribute[]) {
 
     this.id = id
     this.shop = shop
@@ -30,6 +32,7 @@ class Product {
     this.isExisting = isExisting
     this.discountPercent = discountPercent
     this.discountDescription = discountDescription
+    this.attributes = attributes
   }
 
   toJSON() {
---
+++ models/product_attribute.ts
@@ -0,0 +1,11 @@
+class ProductAttribute {
+  name: string
+  value: string
+
+  constructor (name:string, value:string) {
+    this.name = name
+    this.value = value
+  }
+}
+
+export default ProductAttribute
