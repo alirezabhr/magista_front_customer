af189220d73412e1fb4d86baa2573564cefe271c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-21 22:10:55 +0330

    product model changed according to changes in backend models

--- models/product.ts
+++ models/product.ts
@@ -1,38 +1,40 @@
 import ProductAttribute from './product_attribute'
 import Shop from './shop'
+import TagLocation from './tag_location'
 
 class Product {
   id: number
-  shop: Shop
-  finalPrice: number
   originalPrice: number
-  shortcode: string
+  finalPrice: number
   title: string
   description: string
-  displayImageUrl: string
   rate: number | null
   isExisting: boolean
   discountPercent: number | null
+  discountAmount: number | null
   discountDescription: string
   attributes: ProductAttribute[]
+  tag: TagLocation
+  shop: Shop
+  displayImageUrl: string
 
-  constructor(id: number, shop: Shop, finalPrice: number, originalPrice: number, shortcode: string,
-    title: string, description: string, displayImage: string, rate: number | null, isExisting: boolean,
-    discountPercent: number | null, discountDescription: string, attributes: ProductAttribute[]) {
-
+  constructor (id: number, finalPrice: number, title: string, description: string, originalPrice: number,
+    rate: number | null, isExisting: boolean, discountPercent: number | null, discountAmount: number | null,
+    discountDescription: string, attributes: ProductAttribute[], tag: TagLocation, shop: Shop, displayImageUrl: string) {
     this.id = id
-    this.shop = shop
     this.finalPrice = finalPrice
     this.originalPrice = originalPrice
-    this.shortcode = shortcode
     this.title = title
     this.description = description
-    this.displayImageUrl = displayImage
     this.rate = rate
     this.isExisting = isExisting
     this.discountPercent = discountPercent
+    this.discountAmount = discountAmount
     this.discountDescription = discountDescription
     this.attributes = attributes
+    this.tag = tag
+    this.shop = shop
+    this.displayImageUrl = displayImageUrl
   }
 
   toJSON() {
