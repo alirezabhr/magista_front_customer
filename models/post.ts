abc02477b2e02d54bdbb50f0047a43037d59a2fa
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-21 21:50:43 +0330

    post model created

---
+++ models/post.ts
@@ -0,0 +1,20 @@
+import ProductImage from './product_image'
+import Shop from './shop'
+
+class Post {
+  id: number
+  shop: Shop
+  productImages: ProductImage[]
+  shortcode: string
+  description: string
+
+  constructor (id: number, shop: Shop, productImages: ProductImage[], shortcode: string, description: string) {
+    this.id = id
+    this.shop = shop
+    this.productImages = productImages
+    this.shortcode = shortcode
+    this.description = description
+  }
+}
+
+export default Post
