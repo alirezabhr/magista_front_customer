f963ab6ebd186fe2d9a0bdbd84bbe8a16218bf5c
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2021-12-30 15:59:35 +0330

    order model changed totally

--- models/order.ts
+++ models/order.ts
@@ -2,8 +2,6 @@ import OrderItem from './order_item'
 import Shop from './shop'
 import Customer from './customer'
 
-const OrderStatus = ['در انتظار پرداخت', 'پرداخت شده', 'ارسال شده', 'دریافت شده', 'لغو شده']
-
 class Order {
     id: number
     status: number
@@ -12,18 +10,27 @@ class Order {
     customer: Customer
     orderItems: OrderItem[]
     totalPrice: number
-    updatedAt: Date
+    shippedBy: number
+    paidAt: Date
+    verifiedAt: Date
+    shippedAt: Date
+    canceledAt: Date
     createdAt: Date
 
-    constructor (id: number, status: number, shop: Shop, customer: Customer, orderItems: OrderItem[], totalPrice: number, updatedAt: Date, createdAt: Date) {
+    constructor (id: number, status: number, statusText: string, shop: Shop, customer: Customer, orderItems: OrderItem[],
+      totalPrice: number, shippedBy: number, paidAt: Date, verifiedAt: Date, shippedAt: Date, canceledAt: Date, createdAt: Date) {
       this.id = id
       this.status = status
-      this.statusText = OrderStatus[status - 1]
+      this.statusText = statusText
       this.shop = shop
       this.customer = customer
       this.orderItems = orderItems
       this.totalPrice = totalPrice
-      this.updatedAt = updatedAt
+      this.shippedBy = shippedBy
+      this.paidAt = paidAt
+      this.verifiedAt = verifiedAt
+      this.shippedAt = shippedAt
+      this.canceledAt = canceledAt
       this.createdAt = createdAt
     }
 }
