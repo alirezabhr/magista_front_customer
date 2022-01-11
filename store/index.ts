73e51569bb083389152cc76837886e99a893b552
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-11 15:06:52 +0330

    newestProductsList and homePageImagesUrlList added in root Store for homepage

--- store/index.ts
+++ store/index.ts
@@ -1,17 +1,47 @@
-import { GetterTree, MutationTree, ActionTree } from "vuex"
+import { GetterTree, MutationTree, ActionTree } from 'vuex'
+import Product from '~/models/product'
 
 export interface RootState {
+  newestProductsList: Product[]
+  homePageImagesUrlList: String[] 
 }
 
 export const state = (): RootState => ({
+  newestProductsList: [],
+  homePageImagesUrlList: [] 
 })
 
-export const getters: GetterTree<RootState, RootState> = {
-}
-
 export const mutations: MutationTree<RootState> = {
+  setHomePageImagesUrlList (state, imgUrlList) {
+    state.homePageImagesUrlList = imgUrlList
+  },
+  setNewestProductsList (state, products) {
+    state.newestProductsList = products
+  }
 }
 
 export const actions: ActionTree<RootState, RootState> = {
+  homePageImagesUrl (vuexContext) {
+    const url = process.env.baseURL + `homepage-images-url/`
+  
+    return this.$client.get(url).then((response) => {
+      vuexContext.commit('setHomePageImagesUrlList', response.data)
+    })
+  },
+  newestProducts (vuexContext) {
+    const url = process.env.baseURL + `newest-products/`
+  
+    return this.$client.get(url).then((response) => {
+      vuexContext.commit('setNewestProductsList', response.data)
+    })
+  }
 }
 
+export const getters: GetterTree<RootState, RootState> = {
+  getHomePageImagesUrlList: (state): String[] => {
+    return state.homePageImagesUrlList
+  },
+  getNewestProductsList: (state): Product[] => {
+    return state.newestProductsList
+  }
+}
