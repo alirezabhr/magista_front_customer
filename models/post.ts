c30c9edbd4a7cadf79de134ad127928b8d2e0537
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-15 14:41:49 +0330

    product images removed from post model

--- models/post.ts
+++ models/post.ts
@@ -4,14 +4,14 @@ import Shop from './shop'
 class Post {
   id: number
   shop: Shop
-  productImages: ProductImage[]
+  previewImage: string
   shortcode: string
   description: string
 
-  constructor (id: number, shop: Shop, productImages: ProductImage[], shortcode: string, description: string) {
+  constructor (id: number, shop: Shop, previewImage: string, shortcode: string, description: string) {
     this.id = id
     this.shop = shop
-    this.productImages = productImages
+    this.previewImage = previewImage
     this.shortcode = shortcode
     this.description = description
   }
