6584d7fb91db5ec73da89128b7a4b0d0b77c8002
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-22 10:50:32 +0330

    apply Order iscount action and change order mutation implemented

--- store/invoice/index.ts
+++ store/invoice/index.ts
@@ -18,6 +18,12 @@ const state = (): InvoiceState => ({
 const mutations = <MutationTree<InvoiceState>>{
   setSelectedInvoice (state, invoice) {
     state.selectedInvoice = invoice
+  },
+  changeOrder (state, order) {
+    if (state.selectedInvoice) {
+      const orderIndex = state.selectedInvoice.orders.findIndex(el => el.id === order.id)
+      state.selectedInvoice.orders.splice(orderIndex, 1, order)
+    }
   }
 }
 
@@ -44,6 +50,25 @@ const actions = <ActionTree<InvoiceState, RootState>>{
       vuexContext.commit('issue/createNewIssues', null, { root: true })
       const issue = new Issue('paySelectedInvoice', JSON.stringify(e.response))
       issue.setCritical()
+      vuexContext.commit('issue/addIssue', issue, { root: true })
+      vuexContext.dispatch('issue/capture', null, { root: true })
+      throw e.response
+    })
+  },
+  applyOrderDiscount (vuexContext, data) {
+    const orderId = data.orderId
+    const discountCode = data.code
+    const url = process.env.baseURL + `order/${orderId}/discount/apply/`
+
+    const payload = {
+      code: discountCode
+    }
+
+    return this.$client.post(url, payload).then((response) => {
+      vuexContext.commit('changeOrder', response.data)
+    }).catch((e) => {
+      vuexContext.commit('issue/createNewIssues', null, { root: true })
+      const issue = new Issue('applyOrderDiscount', JSON.stringify(e.response.data))
       vuexContext.commit('issue/addIssue', issue, { root: true })
       vuexContext.dispatch('issue/capture', null, { root: true })
       throw e.response
