e972950efc192db401572f52ab6d9b5833fa98b0
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-01-24 19:13:27 +0330

    product title, original price, final price and product discount attributes added in OrderItem

--- models/order_item.ts
+++ models/order_item.ts
@@ -2,10 +2,19 @@ import Product from "./product"
 
 class OrderItem {
   product: Product
+  productTitle: string
+  productOriginalPrice: number
+  productFinalPrice: number
+  productDiscountPercent: number
   count: number
 
-  constructor (product: Product, count = 1) {
+  constructor (product: Product, productTitle: string, productOriginalPrice: number, productFinalPrice: number,
+    productDiscountPercent: number, count = 1) {
     this.product = product
+    this.productTitle = productTitle
+    this.productOriginalPrice = productOriginalPrice
+    this.productFinalPrice = productFinalPrice
+    this.productDiscountPercent = productDiscountPercent
     this.count = count
   }
 
