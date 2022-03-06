1ef023ab7aadf107ae58bf3b707e773a281b90b1
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-06 18:11:11 +0330

    tried to fix problem with navbar in ssr mode. it gives vue warn which the client rendered and ssr are not the same

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>  
-    <NavigationBarMobile v-if="$vuetify.breakpoint.smAndDown" />
+    <NavigationBarMobile v-if="isMobileTablet" />
     <NavigationBarDesktop v-else />
   </div>
 </template>
@@ -14,6 +14,21 @@ export default {
   components: {
     NavigationBarMobile,
     NavigationBarDesktop
+  },
+  data () {
+    return {
+      isHydrated: false   // for vue warn and problems about the differences between ssr and client side rendering
+    }
+  },
+  computed: {
+    isMobileTablet () {
+      return this.isHydrated
+        ? this.$vuetify.breakpoint.smAndDown
+        : false
+    }
+  },  
+  mounted () {
+    this.isHydrated = true
   }
 }
 </script>
