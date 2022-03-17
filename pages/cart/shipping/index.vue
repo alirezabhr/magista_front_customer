26f38e354ebb9840babad6f61a3c31a0c4c66cde
Alireza Bahrololoom <alirezabahrololoom@gmail.com>
2022-03-17 20:19:27 +0330

    bug fixed. cart shipping page will not rendered in server side. needs some data from localStorage

--- pages/cart/shipping/index.vue
+++ pages/cart/shipping/index.vue
@@ -7,17 +7,17 @@
       {{ snackbarMessage }}
     </v-snackbar>
     <v-col cols="12" sm="9" md="8" lg="6" class="pa-0">
-      <v-card min-height="620" outlined color="grey lighten-4" class="pa-2">
+      <v-card v-if="isClientRendered" min-height="620" outlined color="grey lighten-4" class="pa-2">
         <v-card v-for="shopCartOrder in getCart" :key="shopCartOrder.shopId" class="pa-2 my-2">
           <v-card-title>
             <v-col cols="3">
-                  <v-avatar>
-                    <img
-                      :src="shopProfileImageUrl(shopCartOrder.shop.profilePic)"
-                      :aspect-ratio="1"
-                      alt="پروفایل"
-                    >
-                  </v-avatar>
+              <v-avatar>
+                <img
+                  :src="shopProfileImageUrl(shopCartOrder.shop.profilePic)"
+                  :aspect-ratio="1"
+                  alt="پروفایل"
+                >
+              </v-avatar>
             </v-col>
             <v-col cols="9">
               <div>{{ shopCartOrder.shop.instagramUsername }}</div>
@@ -27,11 +27,13 @@
               </div>
             </v-col>
           </v-card-title>
-          <v-card-text v-if="!shopCartOrder.shop.delivery.sendEverywhere && shopCartOrder.shop.city !== getCustomer.city" class="red--text py-0">
+
+          <v-card-text v-show="!shopCartOrder.shop.delivery.sendEverywhere && !inSameLocation(shopCartOrder.shop.province, shopCartOrder.shop.city)" class="red--text py-0">
             این فروشگاه فقط به {{ shopCartOrder.shop.city }} ارسال دارد.
           </v-card-text>
 
           <v-card-subtitle class="pb-1 pt-3">روش ارسال را انتخاب کنید.</v-card-subtitle>
+          
           <div class="my-0 mx-4">
             <v-card
               v-if="shopCartOrder.shop.delivery.nationalPost"
@@ -42,11 +44,15 @@
             >
               <v-row align="center" no-gutters>
                 <v-col offset="1" cols="1">
-                  <v-card height="16" width="16" rounded="circle" :color="shopCartOrder.deliveryId === shopCartOrder.shop.delivery.nationalPost.id ? '#42A5F5' : ''" elevation="0" style="border: 1px solid #42A5F5;" />
+                  <v-card height="18" width="18" rounded="circle" :color="shopCartOrder.deliveryId === shopCartOrder.shop.delivery.nationalPost.id ? '#42A5F5' : ''" elevation="0" style="border: 1px solid #42A5F5;" />
                 </v-col>
                 <v-col class="justify-start" offset="1" cols="9">
                   <div>پست پیشتاز</div>
-                  <div class="text-caption">هزینه حدودی: <span class="font-weight-bold">{{ shopCartOrder.shop.delivery.nationalPost.base }} تومان</span></div>
+                  <div class="text-caption">
+                    هزینه حدودی: 
+                    <span v-if="inSameLocation(shopCartOrder.shop.province, shopCartOrder.shop.city)" class="font-weight-bold">رایگان</span>
+                    <span v-else class="font-weight-bold">{{ shopCartOrder.shop.delivery.nationalPost.base }} تومان</span>
+                  </div>
                 </v-col>
               </v-row>
             </v-card>
@@ -59,11 +65,15 @@
             >
               <v-row align="center" no-gutters>
                 <v-col offset="1" cols="1">
-                  <v-card height="16" width="16" rounded="circle" :color="shopCartOrder.deliveryId === shopCartOrder.shop.delivery.onlineDelivery.id ? '#42A5F5' : ''" elevation="0" style="border: 1px solid #42A5F5;" />
+                  <v-card height="18" width="18" rounded="circle" :color="shopCartOrder.deliveryId === shopCartOrder.shop.delivery.onlineDelivery.id ? '#42A5F5' : ''" elevation="0" style="border: 1px solid #42A5F5;" />
                 </v-col>
                 <v-col class="justify-start" offset="1" cols="9">
                   <div>پیک آنلاین</div>
-                  <div class="text-caption">هزینه حدودی: <span class="font-weight-bold">{{ shopCartOrder.shop.delivery.onlineDelivery.base }} تومان</span></div>
+                  <div class="text-caption">
+                    هزینه حدودی:
+                    <span v-if="inSameLocation(shopCartOrder.shop.province, shopCartOrder.shop.city)" class="font-weight-bold">رایگان</span>
+                    <span v-else class="font-weight-bold">{{ shopCartOrder.shop.delivery.onlineDelivery.base }} تومان</span>
+                  </div>
                 </v-col>
               </v-row>
             </v-card>
@@ -77,12 +87,13 @@
           </div>
         </v-card>
         
-        <v-card flat outlined class="mx-5 my-2 pa-3" v-if="getCustomer">
+        <v-card flat outlined class="mx-5 my-2 pa-3" v-show="getCustomer">
           <v-row no-gutters>
             <v-icon>mdi-map-marker-outline mdi-18px</v-icon>
-            <v-row class="text-caption" no-gutters>{{ getCustomer.province }}-{{ getCustomer.city }}، {{ getCustomer.address }}</v-row>
+            <div class="text-caption" no-gutters>{{ customerFullAddress }}</div>
           </v-row>
         </v-card>
+
         <v-card-actions>
           <v-btn
             class="darken-1 white--text px-4 mx-auto"
@@ -97,6 +108,9 @@
           </v-btn>
         </v-card-actions>
       </v-card>
+      <v-card v-else min-height="620" outlined color="grey lighten-4" class="pa-2">
+        <v-skeleton-loader type="image@2, card-heading" />
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -110,17 +124,43 @@ export default {
     return {
       isSubmitting: false,
       snackbarMessage: '',
-      showSnackbar: false
+      showSnackbar: false,
+      isClientRendered: false
     }
   },
   computed: {
     ...mapGetters('cart', ['getCart']),
-    ...mapGetters('auth', ['getCustomer'])
+    ...mapGetters('auth', ['getCustomer']),
+
+    customerProvince () {
+      if (this.getCustomer) {
+        return this.getCustomer.province
+      }
+      return null
+    },
+    customerCity () {
+      if (this.getCustomer) {
+        return this.getCustomer.city
+      }
+      return null
+    },
+    customerFullAddress () {
+      if (this.getCustomer) {
+        return `${this.getCustomer.province}-${this.getCustomer.city}، ${this.getCustomer.address}`
+      }
+      return ''
+    }
+  },
+  mounted () {
+    this.isClientRendered = true
   },
   methods: {
     ...mapMutations('cart', ['changeShopCartOrderDelivery']),
     ...mapActions('cart', ['createCartOrders']),
     
+    inSameLocation (shopProvince, shopCity) {
+      return this.customerProvince === shopProvince && this.customerCity === shopCity
+    },
     changeDelivery(shopIdN, deliveryIdN) {
       this.changeShopCartOrderDelivery({shopId: shopIdN, deliveryId: deliveryIdN})
     },
